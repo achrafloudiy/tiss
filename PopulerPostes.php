3fb4c3ff277c3ba65bc9d0388156e9e813723c3e
Achraf Loudiy <achrafloudiy@gmail.com>
2019-12-03 23:08:48 -0500

    bug job listings reglé

--- PopulerPostes.php
+++ PopulerPostes.php
@@ -1,12 +1,12 @@
 <?php
-  $sql = "SELECT * FROM emploi";
+  $sql = "SELECT emploi.*, metier.Nom FROM emploi INNER JOIN metier ON emploi.ID_Metier=metier.ID";
   $result = $db->query($sql);
   if ($result->num_rows > 0) {
-
+    //bug gérer nom et id emploi
       while ($row = mysqli_fetch_array($result)){
         echo '
         <tr>
-          <td><a href=""><h2>'.$row['Titre'].'</h2></a><strong>Domaine: </strong>'.$row['ID_Domaine'].'</td>
+          <td><a href=""><h2>'.$row['Nom'].'</h2></a><strong>Domaine: </strong>'.$row['ID_Domaine'].'</td>
           <td>'.$row['Lieu'].'</td>
           <td>'.$row['Statut'].'</td>
           <td><strong>'.$row['Salaire'].'</strong></td>
